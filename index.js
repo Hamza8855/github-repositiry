3a5ecc0e2db5a3f46938869a48724b9c802c34f5
Hamza Farooq <hamzafarooq6650@gmail.com>
2025-10-28 21:32:30 -0700

    second comment

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const name = "hamza";
 
-let myName = "Ahamed";
-console.log(myName);
+let myName1 = "Ahamed";
+console.log(myName1);
+
+
+const myMame = ()=>{
+    console.log("ok h ")
+}
+
+function age(){
+    return myMame();
+}
+
+age();
+let gg = "hj";
